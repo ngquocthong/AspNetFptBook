3d08431207b11e1603be156acf92d91617562d1f
tuanchou <tuantran13005@gmail.com>
2023-02-20 13:24:44 +0700

    CRUD Order

---
+++ BookManagementAPI/Controllers/OrdersController.cs
@@ -0,0 +1,57 @@
+﻿using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+using Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        IOrderRepository repository = new OrderRepository();
+        // GET: api/<OrdersController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Order>> Get() => repository.GetOrders();
+
+        // GET api/<OrdersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<OrdersController>
+        [HttpPost]
+        public IActionResult Post(Order o)
+        {
+            repository.AddOrder(o);
+            return NoContent();
+        }
+
+        // PUT api/<OrdersController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Order c)
+        {
+            var temp = repository.GetOrderByID(id);
+            if (c == null) return NotFound();
+            repository.UpdateOrder(c);
+            return NoContent();
+        }
+
+        // DELETE api/<OrdersController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var c = repository.GetOrderByID(id);
+            if (c == null)
+            {
+                return NotFound();
+
+            }
+            repository.DeleteOrder(c);
+            return NoContent();
+        }
+    }
+}
---
+++ Repositories/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using BusinessObjects;
+using DataAccess;
+
+namespace Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public List<Order> GetOrders() => OrderDAO.GetOrder();
+        public void AddOrder(Order o) => OrderDAO.SaveOrder(o);
+        public void DeleteOrder(Order o) => OrderDAO.DeleteOrder(o);
+
+        public Order GetOrderByID(int id) => OrderDAO.FindOrderById(id);
+
+        public void UpdateOrder(Order o) => OrderDAO.UpdateOrder(o);
+    }
+}
