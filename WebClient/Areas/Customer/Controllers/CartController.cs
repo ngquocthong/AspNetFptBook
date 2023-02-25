6f9fc6a3f5aca5884379ac5c98b5485c47c73877
Ngoãn Huỳnh Văn <huynhngoan389@gmail.com>
2023-02-25 15:35:37 +0700

    Add Cart View

--- WebClient/Areas/Customer/Controllers/CartController.cs
+++ WebClient/Areas/Customer/Controllers/CartController.cs
@@ -1,104 +1,123 @@
-﻿using System.Security.Cryptography;
+﻿using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text.Json;
 using BusinessObjects;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using WebClient.Areas.Customer.Controllers;
 using WebClient.Models;
 
-namespace WebClient.Controllers
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace WebClient.Areas.Customer.Controllers
 {
-    [Route("/books")]
-    public class CartController : Controller
-    {
-        private readonly ILogger<BookController> _logger;
-        private readonly ApplicationDbContext? _context;
-        public CartController(ILogger<BookController>? logger, ApplicationDbContext? context)
-        {
-            _logger = logger;
-            _context = context;
-        }
-        public const string CARTKEY = "cart";
+	[Area("Customer")]
+	public class CartController : Controller
+	{
+		public const string CARTKEY = "cart";
 
-        List<CartItem> GetCartItems()
-        {
-            var session = HttpContext.Session;
-            string jsoncart = session.GetString(CARTKEY);
-            if(jsoncart != null)
-            {
-                return JsonConvert.DeserializeObject<List<CartItem>>(jsoncart);
-            }
-            return new List<CartItem>();
-        }
-        //Clear session of Cart
-        void ClearCart()
-        {
-            var session = HttpContext.Session;
-            session.Remove(CARTKEY);
-        }
-        void SaveCartSession(List<CartItem> ls)
-        {
-            var session = HttpContext.Session;
-            string jsoncart = JsonConvert.SerializeObject(ls);
-            session.SetString(CARTKEY, jsoncart);
-        }
-        [Route("/cart", Name = "cart")]
-        
-        public IActionResult Cart()
-        { 
-            return View();
-        }
+		private readonly HttpClient client = null;
+		private string api;
+		private readonly ILogger<BookController> _logger;
+		public CartController(ILogger<BookController>? logger)
+		{
 
-        // GET: CartController/Create
-        [Route("addcart/{bookid:int}", Name = "addcart")]
-        public ActionResult AddToCart(int bookid)
-        {   
-            var book = _context.Books
-                        .Where(b => b.ID== bookid)
-                        .FirstOrDefault();
-            if (book == null) return NotFound("Not Found books");
+			client = new HttpClient();
+			var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+			client.DefaultRequestHeaders.Accept.Add(contentType);
+			api = "https://localhost:7186/api/Book";
+			_logger = logger;
+		}
 
-            var cart = GetCartItems();
-            var cartitem = cart.Find(b => b.book.ID == bookid);
-            if(cartitem != null)
-            {
-                cartitem.quantity++;
-            } else
-            {
-                cart.Add(new CartItem() { quantity = 1, book = book });
-            }
-            SaveCartSession(cart);
-            return RedirectToAction(nameof (Cart));
-        }
+		List<CartItem> GetCartItems()
+		{
+			var session = HttpContext.Session;
+			string jsoncart = session.GetString(CARTKEY);
+			if (jsoncart != null)
+			{
+				return JsonConvert.DeserializeObject<List<CartItem>>(jsoncart);
+			}
+			return new List<CartItem>();
+		}
+		//Clear session of Cart
+		void ClearCart()
+		{
+			var session = HttpContext.Session;
+			session.Remove(CARTKEY);
+		}
+		void SaveCartSession(List<CartItem> ls)
+		{
+			var session = HttpContext.Session;
+			string jsoncart = JsonConvert.SerializeObject(ls);
+			session.SetString(CARTKEY, jsoncart);
+		}
+		[Route("/cart", Name = "cart")]
 
-        // GET: CartController/Edit/5
-        [Route ("/updatecart", Name = "updatecart")]
-        [HttpPost]
-        public ActionResult UpdateCart(int id, int quantity)
-        {
-            var cart = GetCartItems();
-            var cartitem = cart.Find (b => b.book.ID == id);
-            if(cartitem != null)
-            {
-                cartitem.quantity = quantity;
-            }
-            SaveCartSession(cart);
-            return Ok();
-        }
+		public IActionResult Cart()
+		{
+			return View(GetCartItems());
+		}
+
+		// GET: CartController/Create
+
+		public async Task<ActionResult> AddToCart(int id)
+		{
+			HttpResponseMessage response = await client.GetAsync(api + "/" + id);
+			if (response.IsSuccessStatusCode)
+			{
+				var data = response.Content.ReadAsStringAsync().Result;
+				var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+				var obj = JsonSerializer.Deserialize<Book>(data, options);
+				var book = obj;
+				var cart = GetCartItems();
+				var cartitem = cart.Find(b => b.book.ID == id);
+				if (cartitem != null)
+				{
+					cartitem.quantity++;
+				}
+				else
+				{
+					cart.Add(new CartItem() { quantity = 1, book = book });
+				}
+				SaveCartSession(cart);
+				return RedirectToAction(nameof(Cart));
+			}
+			return NotFound();
 
 
-        [Route("/removecart/{productid:int}", Name = "removecart")]
-        public IActionResult RemoveCart( int id)
-        {
-            var cart = GetCartItems();
-            var cartitem = cart.Find(b => b.book.ID == id);
-            if (cartitem != null)
-            {
-                cart.Remove(cartitem);
-            }
 
-            SaveCartSession(cart);
-            return RedirectToAction(nameof(Cart));
-        }
-    }
+
+
+		}
+
+		// GET: CartController/Edit/5
+		[Route("/updatecart", Name = "updatecart")]
+		[HttpPost]
+		public ActionResult UpdateCart(int id, int quantity)
+		{
+			var cart = GetCartItems();
+			var cartitem = cart.Find(b => b.book.ID == id);
+			if (cartitem != null)
+			{
+				cartitem.quantity = quantity;
+			}
+			SaveCartSession(cart);
+			return Ok();
+		}
+
+
+		[Route("/removecart/{productid:int}", Name = "removecart")]
+		public IActionResult RemoveCart(int id)
+		{
+			var cart = GetCartItems();
+			var cartitem = cart.Find(b => b.book.ID == id);
+			if (cartitem != null)
+			{
+				cart.Remove(cartitem);
+			}
+
+			SaveCartSession(cart);
+			return RedirectToAction(nameof(Cart));
+		}
+	}
 }
