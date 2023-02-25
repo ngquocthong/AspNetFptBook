f978fef16b58966d9baf4056dc68d63139c29d1c
tuanchou <tuantran13005@gmail.com>
2023-02-25 20:00:10 +0700

    Change the confirm email status

--- WebClient/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ WebClient/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -44,8 +44,8 @@ namespace WebClient.Areas.Identity.Pages.Account
             }
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
-            //var result = await _userManager.ConfirmEmailAsync(user, code);
-            //StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
             return Redirect("/Customer/Book/");
         }
     }
